c42b9ae8303583300ce9167048734a13da4d1b57
Nick Groos <ntgroos@gmail.com>
2015-05-17 18:26:54 -0500

    Added XML Parsing example
    also improved comments

--- dbPlay/ViewController.swift
+++ dbPlay/ViewController.swift
@@ -13,21 +13,106 @@ class ViewController: UIViewController, NSXMLParserDelegate {
     @IBOutlet weak var dbOutput: UILabel!
     
     let databasePath = "/Users/ntgroos/ctaapp/cta.sqlite"
-    let ctaURL = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=25924988075841f2970d3e7f95c8070c&mapid=40380&max=5"
     
     let url = NSURL(string: "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=25924988075841f2970d3e7f95c8070c&mapid=40380&max=5")
     
+    
+    // these will be used throughout parsing
+    var posts = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var staID = NSMutableString() // basically used as a stringbuilder for this attribute
+    var arrT = NSMutableString() // basically used as a stringbuilder for this attribute
+
+    
+    // custom function to set up and call superclass parser methods (below)
     @IBAction func myParse(){
+        posts = []
         var xmlParser = NSXMLParser(contentsOfURL: url)
         xmlParser?.delegate = self
         xmlParser?.parse()
+        showParsedResults()
     }
     
-    // This gets called throughout the parsing, can add elements/attributes to a dictionary
-    // need to figure out how to manage nesting of elements vs attributes
-    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!) {
-        dbOutput.text = elementName
+    // gets parsed data and puts it on screen
+    // call this once the parsing is finished, and access dictionaries of data
+    func showParsedResults(){
+        if posts.count > 0{
+            var obj = posts[0] as NSMutableDictionary
+            var arrival = obj["arrT"] as NSString
+            dbOutput.text = arrival
+        }
     }
+    
+    /* Here is the xml object we want.
+    or paste this in browser: http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=25924988075841f2970d3e7f95c8070c&mapid=40380&max=5
+    
+    <eta>
+        <staId>40380</staId>
+        <stpId>30074</stpId>
+        <staNm>Clark/Lake</staNm>
+        <stpDe>Service at Inner Loop platform</stpDe>
+        <rn>015</rn>
+        <rt>G</rt>
+        <destSt>30057</destSt>
+        <destNm>Ashland/63rd</destNm>
+        <trDr>5</trDr>
+        <prdt>20150517 17:20:12</prdt>
+        <arrT>20150517 17:23:12</arrT>
+        <isApp>0</isApp>
+        <isSch>0</isSch>
+        <isDly>0</isDly>
+        <isFlt>0</isFlt>
+        <flags/>
+        <lat>41.88556</lat>
+        <lon>-87.65292</lon>
+        <heading>89</heading>
+    </eta>
+    */
+    
+    // First superclass parser function that gets called. Stop it on the object name you want (eta object is below)
+    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!) {
+        
+        // store current element name in outer class
+        // so it can referenced from other parser method
+        element = elementName
+        
+        if (elementName as NSString).isEqualToString("eta"){
+            elements = NSMutableDictionary.alloc()
+            elements = [:]
+            staID = NSMutableString.alloc()
+            staID = ""
+            arrT = NSMutableString.alloc()
+            arrT = ""
+        }
+    }
+    
+    // Second parser func that gets called throughout.
+    // gets actual data inside tags
+    func parser(parser: NSXMLParser!, foundCharacters string: String!) {
+        if element.isEqualToString("staID"){
+            staID.appendString(string)
+        } else if element.isEqualToString("arrT"){
+            arrT.appendString(string)
+        }
+    }
+    
+    // the last parser fun that gets called. 
+    // called when finding a closing element. Adds the finished strings to dictionary
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!)
+    {
+        if (elementName as NSString).isEqualToString("eta") {
+            if !staID.isEqual(nil) {
+                elements.setObject(staID, forKey: "staID")
+            }
+            if !arrT.isEqual(nil) {
+                elements.setObject(arrT, forKey: "arrT")
+            }
+            
+            posts.addObject(elements)
+        }
+    }
+    
     
     // sends a request to cta REST api
     @IBAction func getSomeXML(sender: AnyObject){
