79a0d9079e723912693fb36248a422bf76bc2825
PaulDHawk <hawkpaul@gmail.com>
2015-05-18 12:27:57 -0500

    added train request api and its test (not implemented all needed info available

---
+++ dbPlayTests/ctaRequestModelTest.swift
@@ -0,0 +1,37 @@
+//
+//  ctaRequestModelTest.swift
+//  dbPlay
+//
+//  Created by paul hawk on 5/18/15.
+//  Copyright (c) 2015 DePaul University. All rights reserved.
+//
+
+import Foundation
+
+ //------------ test arrivals ---------------//
+
+    let tr = ctaRequestModel()
+
+    var mid     : Int?  = 40380
+    var sid     : Int?      // map or stp reqired
+    var maxRt   : Int?      // optional
+    var rts     : Int?      // optional
+
+    let arivalHttpActual = tr.trainRequest.arrivals(mapId: mid, stpId: sid, maxRet: maxRt, rt: rts)
+
+    let arivalHttpExpected : String? = "lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=25924988075841f2970d3e7f95c8070c&mapid=40380"
+
+// ------------Follow This Train API ---------------//
+
+    var followRunNumber : Int?  = 209
+    let followTestActual =  tr.followTrain(followRunNumber)
+    let followTestExpected = "lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=25924988075841f2970d3e7f95c8070c&runnumber=209"
+
+
+// ------------Locations API ---------------//
+
+    var locationsRt : String?  = "Red"         //required
+    let locationsTestActual = tr.locations(locationsRt)
+    let locationsTestExpected = "xx"
+
+
--- dbPlayTests/dbPlayTests.swift
+++ dbPlayTests/dbPlayTests.swift
@@ -34,3 +34,7 @@ class dbPlayTests: XCTestCase {
     }
     
 }
+
+
+
+// ------------Arivals API ---------------//
